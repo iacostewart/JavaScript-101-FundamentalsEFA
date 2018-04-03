57de264f2512604028cc744ec1e41e4066a3f239
Ian Stewart <iacostewart@msn.com>
2018-04-03 00:27:13 -0400

    2.1 if compleated

--- 1-JavaScript-Fundamentals/1-Grammar-and-Types/06-literals.js
+++ 1-JavaScript-Fundamentals/1-Grammar-and-Types/06-literals.js
@@ -1,1 +1,60 @@
 // alert('litterals connected');
+
+/*  
+        ********** Literals ************
+
+        Leterals are fixed valuse within JavaScript. we dont assign them 
+        we insert values directly into the literal. There are 7 types
+        of literals in JavaScript: 
+
+        1) Array Literals
+
+            An Array is a group of valuse, and is denoted by [ ]
+        
+        var = players [ 'Steph Curry', 'Kevin Durant', 'Klay Thompson'];
+
+        2) Boolean Literals
+            ONLY two types. true and false These are diffenent
+            than the primitive types. 
+        
+        var happy =  true;
+
+        3) floating-point Literals
+            Essentally a number with a decimal point, can also include exponents ( similar to scientific notation ) 3.2e12 <--- i have no idea why you would put a e inside of all of this non sense. lol 
+
+        4) Integers 
+            literally numbers. multiple forms, base 10 (0-9), base 8 (octal: 0-7;),
+            base 16 ( hexidecimal; 0-e); or base 2(binary; 0-1);
+
+        5) Object Literals  
+            similar to the object data-type. Example: 
+
+            let x = { a:0, b:1, c:2 }
+                console.log(x.a) // 0 
+                console.log(x.b) // 1
+                console.log(x.c) // 2 
+
+        6) RegExp Literals 
+            RegExp with be explaied further later. its a pattern within slashs, like this: 
+                /ab+c/ .. aka witchcraft.. 
+
+        7) String Literals
+            set of characters with either a single qutoation mark or dounble. 
+            either can be used for a string, but they cannot be used together.. so pick one.. 
+
+        ********* Practice **********
+
+        1) create and array lieteral with 3 items and a object literal with three values. 
+
+        2) convert the hexidecimal number ee to base 10 ( decimal). hint: 0e = 15 
+
+
+// */
+// let x = [
+//     'tooth paste', 'tooth brush', 'dental floss'
+// ];
+
+// let w = {
+//     soup: 'cream of mushroom', salad: 'ceasar', crouton: 'crunchy'
+// };
+
---
+++ 1-JavaScript-Fundamentals/2-ControlFlow-and-ErrorHandling/01-if.js
@@ -0,0 +1,56 @@
+alert('connected')
+ /* 
+                *********** if *************
+
+        in code we have conditionals, we are starting with the if conditional 
+basicly put that conditionals are waiting for some condition to be met. once that is fullfilled the conditional is triggered. 
+
+
+ 
+ */ 
+
+ //Example: 
+
+    var roomClean = true;
+
+    if(roomClean) {
+        console.log("i can take the Ferrari out!");
+    }
+
+    // simple isent it.. this is asking a simple question, over and over again. once its conditions are met, it triggers. In the first 
+    //case it was just a console.log. 
+
+
+        // Practice// 
+    /*
+        see if we can write 5 if statements in about 5 min. 
+
+*/
+var raining = true;
+var snowing = false;
+var morning = true;
+var afternoon = false;
+var warm = false;
+var cold = false;
+
+if(raining) {
+    console.log("dont forget your umbrella");
+}
+if(snowing) {
+    console.log("Need to wear your boots.. its snowing");
+}
+if(morning) {
+    console.log('good Morning, hope you have a great day');
+}
+if(afternoon) {
+    console.log('good afternoon, hope your day has gone well');
+}
+if(warm) {
+    console.log('dress lightly, its warm outside');
+}
+if(cold) {
+    console.log('its best to layer-up.. its cold outside');
+}
+if(morning && raining) {
+    console.log('good morning, better pack your umbrella its raining outside');
+}
