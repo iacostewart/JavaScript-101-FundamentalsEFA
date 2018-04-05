54f0300f6a6a71738d2cdb69126050fba8d0653b
Ian Stewart <iacostewart@msn.com>
2018-04-05 11:49:23 -0400

    added 01-if

--- 1-JavaScript-Fundamentals/2-ControlFlow-and-ErrorHandling/01-if.js
+++ 1-JavaScript-Fundamentals/2-ControlFlow-and-ErrorHandling/01-if.js
@@ -11,11 +11,11 @@ basicly put that conditionals are waiting for some condition to be met. once tha
 
  //Example: 
 
-    var roomClean = true;
+    // var roomClean = true;
 
-    if(roomClean) {
-        console.log("i can take the Ferrari out!");
-    }
+    // if(roomClean) {
+    //     console.log("i can take the Ferrari out!");
+    // }
 
     // simple isent it.. this is asking a simple question, over and over again. once its conditions are met, it triggers. In the first 
     //case it was just a console.log. 
@@ -25,32 +25,32 @@ basicly put that conditionals are waiting for some condition to be met. once tha
     /*
         see if we can write 5 if statements in about 5 min. 
 
-*/
-var raining = true;
-var snowing = false;
-var morning = true;
-var afternoon = false;
-var warm = false;
-var cold = false;
+// */
+// var raining = true;
+// var snowing = false;
+// var morning = true;
+// var afternoon = false;
+// var warm = false;
+// var cold = false;
 
-if(raining) {
-    console.log("dont forget your umbrella");
-}
-if(snowing) {
-    console.log("Need to wear your boots.. its snowing");
-}
-if(morning) {
-    console.log('good Morning, hope you have a great day');
-}
-if(afternoon) {
-    console.log('good afternoon, hope your day has gone well');
-}
-if(warm) {
-    console.log('dress lightly, its warm outside');
-}
-if(cold) {
-    console.log('its best to layer-up.. its cold outside');
-}
-if(morning && raining) {
-    console.log('good morning, better pack your umbrella its raining outside');
-}
+// if(raining) {
+//     console.log("dont forget your umbrella");
+// }
+// if(snowing) {
+//     console.log("Need to wear your boots.. its snowing");
+// }
+// if(morning) {
+//     console.log('good Morning, hope you have a great day');
+// }
+// if(afternoon) {
+//     console.log('good afternoon, hope your day has gone well');
+// }
+// if(warm) {
+//     console.log('dress lightly, its warm outside');
+// }
+// if(cold) {
+//     console.log('its best to layer-up.. its cold outside');
+// }
+// if(morning && raining) {
+//     console.log('good morning, better pack your umbrella its raining outside');
+// }
