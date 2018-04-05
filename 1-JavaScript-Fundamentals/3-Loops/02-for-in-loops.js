b04d615f48ea522c4b761e9d00ce8f1fbf7281c9
Ian Stewart <iacostewart@msn.com>
2018-04-05 15:31:05 -0400

    for-in-loop

---
+++ 1-JavaScript-Fundamentals/3-Loops/02-for-in-loops.js
@@ -0,0 +1,27 @@
+/*
+     ********** for in Loops *********
+
+     the For in Statment is primarily to iterate variables over the enumerable properties. 
+     the For in loop executes specific statments. Its best used for looping over objects. 
+     to loop over an arry we often need to use the index, to get the value we want. 
+
+
+*/
+
+// for (var i /* variable section */ in obj /* object section */ ) {
+//     console.log(i) //statement 
+
+// }
+
+// write a for loop that captalizes the first letter of studentName 
+
+var studentName = "paul"; 
+var capSN = '';
+for ( var n in studentName) {
+    if (n == 0) {
+        capSN = studentName[n].toUpperCase();
+    } else {
+        capSN += studentName[n];
+    }
+}
+console.log(capSN);
