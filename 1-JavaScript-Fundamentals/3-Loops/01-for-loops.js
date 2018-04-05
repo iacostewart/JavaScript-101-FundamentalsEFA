856c1a44111e949483a6f4091a0e5fa65b3433e3
Ian Stewart <iacostewart@msn.com>
2018-04-05 14:02:52 -0400

    01-for-loops

---
+++ 1-JavaScript-Fundamentals/3-Loops/01-for-loops.js
@@ -0,0 +1,39 @@
+/*  
+        ******** For Loops **********
+
+    The four Statment Loop repeats untill a specific condition is FALSE.    
+    these are the steps of what happens when a for statment is running: 
+    1.) Variable/Index section is executed (i.e var i = 1)
+
+    2.) the conditional section is evaluated (i.e 1<= 10 )
+    if the value is true the loop continues to run. 
+    if the conditional section is omitted, the default is TRUE. 
+        ** note ommitting the conditional statment may result in an infinite loop. 
+        
+    3.) the statement executes ( i.e console.log('number:', i);).
+    4.) the increment section is excuted (i.e i++). 
+
+*/
+
+//for statment loop
+
+// for ( var i = 1 /*variable/index/  section*/; i <= 10/* conditional section */; i++ /* increment section */ ) {
+//     console.log("number:", i );
+
+// }
+// alert('connected');
+// practice 
+// for (var i = 0; i<= 50; i+=5 ) {
+//     console.log(i);
+
+// }
+
+// for (var i = 20; i > 1; i--) {
+//     console.log(i);
+// }
+
+
+// write a for loop that starts at 3, and increments by 5 but dosent go over 30.. 
+// for(i = 3; i<30; i+=5) {
+//     console.log(i);
+// }
