dda1898c5ab253047f7d96e4da5df8817a95370f
Ian Stewart <iacostewart@msn.com>
2018-04-05 11:53:04 -0400

    03-Switch

---
+++ 1-JavaScript-Fundamentals/2-ControlFlow-and-ErrorHandling/03-switch.js
@@ -0,0 +1,46 @@
+// alert('connected');
+
+/* 
+                *********** SWITCH **********
+
+                A switch statment lets us evaluate an expression 
+            and compare it to other values called 'cases'. Switch statements are used 
+            heavily in packages called 'Redux', more on that later. 
+
+
+    */
+
+    
+    // sample code  
+
+    // var friend = 'karl'; 
+
+    // switch (friend) {
+    //     case 'Fred':
+    //         console.log('hey fred, lets go golfing.');
+    //         break;
+    //     case 'karl':  // prime example of capital K's... keep you characters in check.
+    //         console.log('lets hang');
+    //         break;
+    //     case 'john' : 
+    //         console.log('Sorry, im busy right now.');
+    //     break; 
+    //         default: 
+    //         console.log('hey ' + friend + ' can i callyou back in a munuet') 
+    // };
+
+    // // practice 
+
+    /*
+            pick one or both: 
+        1 ) its cliche, but it's good to know how to dow it: write a switch that 
+        say what we should wear baised on changing weather. Deal with 0 or lower, 
+        0-32, 32-50... for instnace if its 75, we might get a console statment 
+        that says: 'it's 75 degrees out. you could probally get away with shorts."
+        "
+    */
+
+
+
+
+   
