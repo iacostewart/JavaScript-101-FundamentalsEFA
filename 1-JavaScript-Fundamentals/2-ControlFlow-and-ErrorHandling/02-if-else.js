e68c9f7e4b825ae61a1bc76b0ca83a7698d5a74e
Ian Stewart <iacostewart@msn.com>
2018-04-05 11:50:25 -0400

    02-if-else

---
+++ 1-JavaScript-Fundamentals/2-ControlFlow-and-ErrorHandling/02-if-else.js
@@ -0,0 +1,58 @@
+/*    
+            ********** if else ***********
+
+    Hand in hand with the if statments, there is the if else section of the world. 
+    simplified to the point of "you need to clean your room, or else you will be grounded for ever"
+
+    there are two possible conditions. 
+    room cleaned
+    grounded 
+
+*/
+
+// var age = 17; 
+
+// if(age >= 18) {
+//     console.log("you can vote")
+// }else{
+//     console.log('you can not vote');
+// };
+
+// var elevatorUP = true;
+// var elevatorDown = true;
+// var elevatorBroken = true;
+// var elevatorStuckWhileWeAreOnIt= true;
+// var elevatorNumber = 13;
+
+// if(elevatorUP === true) {
+//     console.log('going up')
+// }else{
+//     console.log("going down");
+// }
+
+// if(elevatorBroken){
+//     console.log('bummer. lets take the stairs')
+// }else{
+//     console.log('which floor?')
+// };
+
+// if(elevatorNumber === 13 && elevatorStuckWhileWeAreOnIt){
+//     console.log('this is not your lucky day')
+// };
+
+// // ***** challenge #2 ********
+
+
+// //1) fix the banks bad code... 
+
+// // var bankAccount = 10000;
+// // var debt = 4200;
+// // var difference = bankAccount - debt;
+
+// // console.log("i have $" + bankAccount, " in my bank account, and i am $" + debt, " in debt." );
+
+// // if ((bankAccount - debt > 700)) {
+// //     console.log("i have extra mone. I should pay off my debts")
+// // }else {
+// //     console.log("it probally isent a good time to pay off my debts")
+// // };
