c427b05d0ccb1a7f10ca7e878c9941a61a74a43f
Ian Stewart <iacostewart@msn.com>
2018-04-05 12:53:55 -0400

    04-try-catch

---
+++ 1-JavaScript-Fundamentals/2-ControlFlow-and-ErrorHandling/04-try-catch.js
@@ -0,0 +1,27 @@
+/*      
+            ******** Try Catch *******
+
+Try BlocK 
+
+The try block will contain one or more statemtents. That is, you want the try block to succeed.
+Like a participation Throphy. If it does not succed you want control to pass the catch block. 
+
+// */ 
+
+// function tryCatchDemo(a) {
+//     try {
+//         var upperCaseWord = a.toUpperCAse();
+//         console.log(upperCaseWord);
+//     }
+//         catch(err) {
+//             console.log(err);
+//         }
+// }
+// tryCatchDemo('hello')
+// tryCatchDemo(7); // 2nd call 
+
+/* 
+        the try catch block is in my opnion something that you place into a function to stop errors from killing the application. essetntally instead of breaking the app, it just console.logs the err message. 
+
+
+*/
