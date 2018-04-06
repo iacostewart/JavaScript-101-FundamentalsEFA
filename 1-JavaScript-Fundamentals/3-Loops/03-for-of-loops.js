65047c8750118051313cc82de7d54dca8e46ba9b
Ian Stewart <iacostewart@msn.com>
2018-04-05 20:13:43 -0400

    for-of-loops

---
+++ 1-JavaScript-Fundamentals/3-Loops/03-for-of-loops.js
@@ -0,0 +1,40 @@
+/*
+
+            ********* For Of Loops **********
+
+            For of Statement creates a loop that iterates over iteratble objects.. 
+                    Array 
+                    Map 
+                    Set 
+                    Arguments etc. 
+
+            for of   is particular useful for iterating over other kinds of collections. Cannot iterate over objecs, so for in works in the best for dealing with objects. 
+            for loops where introduced in ES6. they are excelent for iterating over arrays. 
+
+
+            For in     vs        for of 
+
+            for in loops are used best for objects         where for of loops are great for iterating over arrays. 
+
+        take the example. I want an arry of cat names, and i want to console eah name and "says meow". if it try and do it in a for in loops, i wont get what im looking for. Take the below code. 
+
+
+        */
+       
+var catArray = ['tabby', 'british shorthair', 'burmese', 'main coon', 'rag doll'];
+
+for ( var cat of catArray) {
+    if( cat == 'main coon' ) {
+        console.log(cat, 'Says nothing.. Will Eat your Children')
+    } else { 
+        console.log(cat, 'Says meow')
+    } }
+
+
+
+
+    var catArray = ['tabby', 'british shorthair', 'burmese', 'main coon', 'rag doll'];
+
+    for( var cat of catArray) {
+          console.log(cat, 'Says meow')
+}
