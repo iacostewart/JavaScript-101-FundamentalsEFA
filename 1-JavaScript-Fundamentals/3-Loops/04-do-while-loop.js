54e03f9c1d50475a8ecffbed67a2a7eb09882a3b
Ian Stewart <iacostewart@msn.com>
2018-04-05 20:35:40 -0400

    do-while-loops

---
+++ 1-JavaScript-Fundamentals/3-Loops/04-do-while-loop.js
@@ -0,0 +1,33 @@
+/* 
+         Do While Loops 
+
+Do while statements 
+
+the do.... while statemnt loop repeats until a spcified condition
+comes back as FALSE.  
+
+the statment ( i.e i+ 1; ) executes once befor the condition is checked: 
+to execute multiple statments, use a block statement (i.e {......}) to group the statements together. 
+
+if the condition (i.e i < 5 ) is  FALSE, the loop stops and control passes.
+
+*/ 
+// alert('connected');
+
+// var i = 0; 
+
+// do /* do */ {
+//     i += 1; /* statement */
+//     console.log(i);
+// } while (i < 20 ); /* while ( condition ) */ 
+
+
+
+// var text = " "; 
+// var i = 0; 
+
+// do {
+//     text += "The number is: " + i + " ";
+//     i++;
+// } while (i < 10 ); // "The number is: 0", "the number is: 1",...... "the number is: 10";
+// console.log(text);
