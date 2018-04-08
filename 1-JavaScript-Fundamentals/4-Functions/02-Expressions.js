480d03fad04ff6ef1a6c2b279f7d35b02ea6f87d
Ian Stewart <iacostewart@msn.com>
2018-04-08 16:55:37 -0400

    expressions

---
+++ 1-JavaScript-Fundamentals/4-Functions/02-Expressions.js
@@ -0,0 +1,21 @@
+/*   
+         Function Expressions 
+
+         Rules... 
+         1) Can be Anonymous. 
+         2) Cannot be Hoisted 
+
+        
+*/
+
+alert('connected');
+
+fooFunction();
+
+function fooFunction() {
+    console.log('foo');
+}
+
+
+// Practice writing 3 different function expressions of your own chooseing. 
+
