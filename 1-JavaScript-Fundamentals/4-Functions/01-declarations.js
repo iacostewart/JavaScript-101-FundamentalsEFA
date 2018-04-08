efc85ab596d1087acc63cb7b6d0158ab97fe5e99
Ian Stewart <iacostewart@msn.com>
2018-04-08 16:55:04 -0400

    declarations

---
+++ 1-JavaScript-Fundamentals/4-Functions/01-declarations.js
@@ -0,0 +1,47 @@
+// /*  Declarations 
+
+
+// Function declarations or function statemnts are one of the ways to 
+// create a function in javaScript. 
+
+// There area few key rules. 
+// 1)
+//     Declarations Start by using the function keyword. 
+// 2) 
+//     the function gets a name, it can be anything and should be
+//     camel case..
+// 3) 
+//     Add empty parenthesis or parameter names in the parentheis. 
+// 4) 
+//     Put the defining statmens inside {}. 
+// 5) 
+//     can be hoisted. 
+    
+// */
+// alert('connected')
+//  var a= 2;
+//  var b= 2;
+// b=2
+// function addTwoNumbers(a, b){
+//     console.log(a + b);
+//     return a + b; 
+
+// }
+
+// //      Calling the Functions 
+
+// addTwoNumbers(5, 7);
+
+// /*  
+//     1) Practice writeing 3 differnet function declarations. 
+//     the function should subtract number b from number a 
+//     and return the value. log it in the console.
+
+//     2) write a function with tow parameters (first, last). the function should take the two parameters and say hello 
+//     to a user by thier first full name. for instance "hello, Kenn Pascascio". 
+
+//     3) Write a function that has one parameter( minuetsLeft). The message should print, 'You have 5 minuets left in the show."
+
+
+
+// */
