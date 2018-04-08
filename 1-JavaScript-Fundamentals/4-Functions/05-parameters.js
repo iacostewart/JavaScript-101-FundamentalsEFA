7f66d24262b2e70e0555f2f692f553a420a6e724
Ian Stewart <iacostewart@msn.com>
2018-04-08 16:57:44 -0400

    parameters

---
+++ 1-JavaScript-Fundamentals/4-Functions/05-parameters.js
@@ -0,0 +1,23 @@
+ /*
+    A parameter is a variable in a method definition. Other ways to describe it.. 
+    its the variable in the eclaration of the function. and it is found inside the 
+    parenthesis. 
+    
+ */
+
+ // example.. the two parameters are name & type
+
+ function petNameAndBreed(name, type){
+     var petDetails = name + ', ' + type;
+     console.log(petDetails);
+     return petDetails;
+
+ }
+
+ // Key points..  
+ // 1) parameters are like variables. 
+ // 2) declared in the parenthesis of a function. 
+ // 3) the arguments are the valuse that get assigned to the arameters when the function is called. 
+
+
+ 
