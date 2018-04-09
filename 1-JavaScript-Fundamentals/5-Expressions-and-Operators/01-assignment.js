5ae926f3db0b08fefda15a2e19a64fd6db90829e
Ian Stewart <iacostewart@msn.com>
2018-04-09 10:53:40 -0400

    01-assignment

---
+++ 1-JavaScript-Fundamentals/5-Expressions-and-Operators/01-assignment.js
@@ -0,0 +1,20 @@
+/*
+    Assignment 
+
+    Assignment is the process in which a value is placed inside a variable, there are many different assignment operators, mos common is the "=" operator. 
+    like this. 
+
+    let x = 42
+
+    
+    Name	                   Purpose	          Shorthand
+Addition Assignment	           x = x + y	       x += y
+Subtraction Assignment	       x = x - y	       x -= y
+Multiplication Assignment      x = x * y	       x *= y
+Division Assignment	           x = x / y	       x /= y
+Remainder Assignment	       x = x % y	       x %= y
+Exponential Assignment	       x = x ** y          x **= 
+ */
+
+
+// string operators can also be used with strings, such as let x = 'testing'.    String concatentation is another example. 
