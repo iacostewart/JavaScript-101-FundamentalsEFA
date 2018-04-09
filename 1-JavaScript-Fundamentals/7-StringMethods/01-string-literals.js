5e431695539d36e78c611ac495b4ebdf2a7655b2
Ian Stewart <iacostewart@msn.com>
2018-04-09 11:01:05 -0400

    literals_methods

---
+++ 1-JavaScript-Fundamentals/7-StringMethods/01-string-literals.js
@@ -0,0 +1,40 @@
+// STRING LITERALS
+// A string is a data-type in JavaScript that represents text. "This sentence is a string." 'So is this one.' Whenever you log into a website, your username and password are read as strings. Strings are one of the basic building blocks that you will find yourself using over and over and over.
+// How to create a string
+// A string is a group of characters enclosed in quotations. In JavaScript, unlike some other languages, you can use single or double quotes for a string, but they cannot be used together. There are many different ways to create and use strings:
+// let x = "This is a string.";
+// let y = 'This is also a string.';
+// console.log("This is printing a string to the console.");
+// let z = "This is NOT a string, and will cause an error';
+// Strings are used for everything from printing to the console to displaying error messages to asking for input from the user and more.
+// String Concatenation
+// The + operator acts differently with strings than it does with numbers. Rather than add two numbers together, it simply smashes the two strings into one, like so:
+// let a = "StringA"
+// let b = "StringB"
+// console.log(a + b) //"StringAStringB"
+// console.log("Hello " + b) //"Hello StringB"
+// console.log(a + 'text' + b) //"StringAtextStringB"
+// String Indexes
+// Each character in a string is assigned an index, starting with 0 for the first character. Some methods allow you to access a specific string character to add, remove, or change it. The next module will show some of these in action. So in the case of let x = "Hello World!", console.log(x(0)) will print the character "H" to the console.
+// Escaping Strings
+// Occasionally there will be times when you want to include special characters within a string to perform a specific function. Using a backslash (\) within a string tells the compiler that the next character is not part of the string, and to treat it differently. Consider the following:
+// console.log('Mary's father said "I don't want you to work there!"');
+// If you attempt to run this string, it will throw an error because of the apostrophes in the words Mary's and don't. The program things that that is the end of the string. Using the backslash will tell the program to ignore the apostrophe and to allow the code to execute properly:
+// console.log('Mary\'s father said "I don\'t want you to work there!"');
+// File Location
+// We will be working in the following file:
+//     javascript-library
+//         └── 0-PreWork
+//         └── 1-Fundamentals
+//             └── 1-Grammar-and-Types
+//             └── 2-ControlFlow-and-ErrorHandling
+//             └── 3-Loops
+//             └── 4-Functions
+//             └── 5-Expressions-and-Operators
+//             └── 6-Numbers-and-Dates
+//             └── 7-StringMethods
+//                 01-string-literals.js <----You will be working in this file.
+// Practice
+// Create two string variables, print them to the console, then concatenate them together and print the result.
+// Create a variable with the string "STRINGS ARE COOL". Print only the characters that are vowels.
+// Write a short (3-5 sentence) paragraph about anything you want and print it to the console. Escape the string if necessary.
---
+++ 1-JavaScript-Fundamentals/7-StringMethods/02-string-methods.js
@@ -0,0 +1,39 @@
+// STRING METHODS
+// Like other object, the String object has many built-in methods and functions that you can access. Here are some of the most common:
+// Name	Description
+// length	This string property returns the length of the string.
+// charAt()	Returns the character at the given index of the string. If left blank, returns the character at index 0.
+// concat()	Takes an unlimited number of strings, concatenates them to the original string, and returns the result.
+// endsWith():	Returns true if the given character is at the last index of the string, otherwise returns false.
+// includes()	Returns true if the given character is present in the string, otherwise returns false.
+// indexOf()	Returns the first index location of the given character in the string if present. Returns -1 if not found.
+// lastIndexOf()	Returns the last index location of the given character in the string if present. Returns -1 if not found.
+// repeat()	Duplicates the original string the given number of times and returns the result.
+// slice()	Cuts the string in two at the given index and returns the result.
+// startsWith()	Returns true if the given character is at the first index of the string, otherwise returns false.
+// toUpperCase()	Capitalizes each letter in the original string and returns the result.
+// toLowerCase()	Converts each letter in the original string to lower case and returns the result.
+// trim()	Removes white space at either end of the original string and returns the result.
+// You can find more information on these and other String methods here
+// File Location
+// We will be working in the following file:
+//     javascript-library
+//         └── 0-PreWork
+//         └── 1-Fundamentals
+//             └── 1-Grammar-and-Types
+//             └── 2-ControlFlow-and-ErrorHandling
+//             └── 3-Loops
+//             └── 4-Functions
+//             └── 5-Expressions-and-Operators
+//             └── 6-Numbers-and-Dates
+//             └── 7-StringMethods
+//                 01-string-literals.js 
+//                 02-methods.js <----You will be working in this file.
+// Practice
+// Create a string and print it to the console, print the length of the string, then remove the last two characters of the string and print the result.
+// Add the string "HELLO WORLD" to your current string at index 3 and print the result.
+// Create one variable with the string EFA JS March 2018 and a second variable that is a blank string. Split the string at the M in March, put the result into the second variable, and print both to the console.
+// BONUS CHALLENGE
+// Create a string with your first name. Write a function to capitalize only the first letter and print the result to the console.
+// BONUS CHALLENGE 2
+// Create a string with your first and last name. Write a function to capitalize only the first letter of each and return the result
