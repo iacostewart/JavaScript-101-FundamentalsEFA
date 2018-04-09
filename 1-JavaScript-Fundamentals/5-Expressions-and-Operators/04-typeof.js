1e09193e507577c30fb9d7d3f371556cc99270cc
Ian Stewart <iacostewart@msn.com>
2018-04-09 10:55:40 -0400

    typeof

---
+++ 1-JavaScript-Fundamentals/5-Expressions-and-Operators/04-typeof.js
@@ -0,0 +1,22 @@
+/*
+Typeof 
+
+typeof is a reserved keyword in javaScript that does exzctl
+what it says. it returns the typ of the value in question. 
+the proper syntax is typeof x 
+
+consider the following 
+
+let x = 2; 
+let y = 'hello';
+let z = false; 
+
+
+practice 
+1) in typeof.js create several varialbels and give them differnet value types. 
+use typeof on each to pring the type to the conosle.
+
+2) write a program, that has different outcomes depending on the type
+of value used. you can use if/else, switch , or ternary operators. for and extra challenge do all 3. 
+ */
+
