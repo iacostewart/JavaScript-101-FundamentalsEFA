ded3b429f601af8f5ebbada5551742651a28bf93
Ian Stewart <iacostewart@msn.com>
2018-04-11 10:06:56 -0400

    operators

--- 1-JavaScript-Fundamentals/5-Expressions-and-Operators/04-typeof.js
+++ 1-JavaScript-Fundamentals/5-Expressions-and-Operators/04-typeof.js
@@ -20,3 +20,43 @@ use typeof on each to pring the type to the conosle.
 of value used. you can use if/else, switch , or ternary operators. for and extra challenge do all 3. 
  */
 
+
+
+
+
+
+
+
+ 
+
+ var mainStreet = [{houseNumber: 770, basement: true, bedrooms: 3, bathrooms: 1, twoStory: false, fence: true, driveway: 'gravel'},
+              {houseNumnber: 772, basement: false, bedrooms: 5, bathrooms: 2, twoStory: true, fence: false, driveway: 'brick'}];
+
+
+
+
+
+
+
+              console.log(mainStreet[0]);
+
+
+   mainStreet.unshift({houseNumber: 774, basement: true, bedrooms: 5, twoStory: true, fence: false, driveway: 'asphalt' })
+
+
+
+
+
+
+   for (let i = 0; i < mainStreet.length; i++)
+{
+    console.log(mainStreet[i]); 
+}
+
+var indOf = [];
+
+console.log(indOf.indexOf(1));
+
+
+var abc = ['a','b','c','d'];
+console.log(abc.indexOf());
